e968e53c91f6371e60b8f14bf11b61f0855dc5f8
Russell Sutter <russell.sutter@gmail.com>
2020-10-05 20:57:41 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,26 +1,29 @@
+  
 import React, { Component } from 'react';
 
-
 export default class Cell extends Component {
-
-    constructor(props) {
-        super()
-        this.state = {
-            color: props.value
-        }
+  
+  constructor(props) {
+    super()
+    this.state = {
+      color: props.value
     }
-
-    clickHandler = () => {
-        this.setState({
-            color: '#333'
-        })
-    }
-
-    render() {
-        return <div 
-          className="cell" 
-          style={{ backgroundColor: this.state.color }} 
-          onClick={this.clickHandler}>
-        </div>
-    }
+  }
+  
+  handleClick = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+  
+  render() {
+    return (
+      <div 
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
+      </div>
+    )
+  }
+  
 }
